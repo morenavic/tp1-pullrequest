8c1b2397194bf2d36dae50166ee74fe78b6d4df3
Morena Gallardo <morevg.996@gmail.com>
2025-03-28 20:17:55 -0300

    Segunda modificación a los archivos.

--- hola-mundo.java
+++ hola-mundo.java
@@ -1,6 +1,8 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo!");
-        System.out.println("/n ¡Hola GIT!");
+        String saludoJava="¡Hola Java!";
+        String saludoGit="¡Hola GIT!";
+        System.out.println(saludoJava);
+        System.out.println(saludoGit);
     }
 }
