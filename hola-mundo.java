05ac1d33dcb19b47ac77556e20472570d95f0956
Morena Gallardo <morevg.996@gmail.com>
2025-03-31 13:01:15 -0300

    Refactorización (unificar println en una sola llamada)

--- hola-mundo.java
+++ hola-mundo.java
@@ -2,8 +2,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         String saludoJava="¡Hola Java!";
         String saludoGit="¡Hola GIT!";
-        System.out.println(saludoJava);
-        System.out.println("/n");
-        System.out.println(saludoGit);
+        System.out.println(saludoJava + "\n\n" + saludoGit);
     }
 }
